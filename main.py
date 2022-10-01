465f0aa1b1531cb3a668b24342b2df7eafed3510
HoneyBerman <honeyoberman@gmail.com>
2022-10-01 21:41:41 +0530

    added GUI for generating QR Code

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
 import pyqrcode
 import png
-link = input("Enter The Link: ")
-qr_code = pyqrcode.create(link)
-qr_code.png("code.png", scale=5)
+from tkinter import *
+from PIL import Image, ImageTk
+
+def Generate():
+    if my_entry.get():
+        global qr_code,img
+        qr_code = pyqrcode.create(my_entry.get())
+        img = BitmapImage(data = qr_code.xbm(scale=8))
+        img_lbl.config(image=img)    
+
+root = Tk()
+root.title('QR Code Generator')
+root.geometry("500x500")
+
+my_text=Label(root, text="Enter the Link:")
+my_text.pack(pady=20)
+
+my_entry=Entry(root)
+my_entry.pack(pady=20)
+
+my_button = Button(root,text="QR Code",command=Generate)
+my_button.pack(pady=20)
+
+qr_label=Label(root, text="Your QR Code")
+qr_label.pack(pady=50)
+
+img_lbl = Label(root)
+img_lbl.pack()
+
+root.mainloop()
